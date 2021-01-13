d81f0b4bceab0937976eb62292d1bef2eb6d9053
Jaeyoung Kim <jaeyoung.kim.dev@gmail.com>
2021-01-12 21:11:12 -0700

    Update index.js

--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import Icon1 from '../../images/svg1.svg';
 // import Icon2 from '../../images/svg-2.svg';
 // import Icon3 from '../../images/svg-3.svg';
 import {
