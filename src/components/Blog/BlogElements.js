f4a10567290f0435b857e2e852a2c0fc25a17674
Jaeyoung Kim <50346571+Jaeyoung-Kim-CND@users.noreply.github.com>
2021-01-16 23:01:27 -0700

    Replaced project cards

--- src/components/Blog/BlogElements.js
+++ src/components/Blog/BlogElements.js
@@ -2,21 +2,22 @@ import styled from 'styled-components';
 import { Link } from 'react-scroll';
 
 const backgroundLight = '#f9f9f9';
+const backgroundDark = '#0a0620;';
 const topLine = '#01bf71';
-// const headingDark = '#f7f8fa';
+const headingDark = '#f7f8fa';
 const headingLight = '#010606';
-// const subtitleDark = '#fff';
+const subtitleDark = '#fff';
 const subtitleLight = '#010606';
-// const btnBgDark = '#01BF71';
+const btnBgDark = '#01BF71';
 const btnBgLight = '#010606';
-// const btnTxtDark = '#010606';
+const btnTxtDark = '#010606';
 const btnTxtLight = '#fff';
-// const btnBgHoverDark = '#fff';
+const btnBgHoverDark = '#fff';
 const btnBgHoverLight = '#01BF71';
 
 export const BlogContainer = styled.div`
   color: #fff;
-  background: ${backgroundLight};
+  background: ${backgroundDark};
 
   @media screen and (max-width: 768px) {
     padding: 50px 0;
@@ -84,6 +85,8 @@ export const ImgWrap = styled.div`
 
 export const Img = styled.img`
   width: 100%;
+  border-radius: 10px;
+  box-shadow: 0px 3px 10px rgba(0, 0, 0, 0.5);
 `;
 
 // export const TextWrapper = styled.div`
@@ -106,7 +109,7 @@ export const Heading = styled.h1`
   font-size: 48px;
   line-height: 1.1;
   font-weight: 600;
-  color: ${headingLight};
+  color: ${headingDark};
 
   @media screen and (max-width: 480px) {
     font-size: 32px;
@@ -118,7 +121,7 @@ export const Subtitle = styled.p`
   margin-bottom: 30px;
   font-size: 18px;
   line-height: 24px;
-  color: ${subtitleLight};
+  color: ${subtitleDark};
 `;
 
 export const BtnWrap = styled.div`
@@ -128,10 +131,10 @@ export const BtnWrap = styled.div`
 
 export const Button = styled(Link)`
   border-radius: 50px;
-  background: ${btnBgLight};
+  background: ${btnBgDark};
   white-space: nowrap;
   padding: 12px 30px;
-  color: ${btnTxtLight};
+  color: ${btnTxtDark};
   font-size: 16px;
   outline: none;
   border: none;
@@ -143,6 +146,6 @@ export const Button = styled(Link)`
 
   &:hover {
     transition: all 0.2s ease-in-out;
-    background: ${btnBgHoverLight};
+    background: ${btnBgHoverDark};
   }
 `;
---
+++ src/components/Skills/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {
+  SkillsContainer,
+  SkillsWrapper,
+  SkillsRow,
+  Column1,
+  ImgWrap,
+  Img,
+  Column2,
+  TopLine,
+  Heading,
+  Subtitle,
+  BtnWrap,
+  Button,
+} from './SkillsElements';
+
+const Skills = () => {
+  return (
+    <>
+      <SkillsContainer id={'skills'}>
+        <SkillsWrapper>
+          <SkillsRow>
+            <Column1></Column1>
+            <Column2>
+              <TopLine>Skills</TopLine>
+              <Heading>Skills</Heading>
+              <Subtitle>Skills</Subtitle>
+              <BtnWrap>
+                <Button>Details</Button>
+              </BtnWrap>
+            </Column2>
+          </SkillsRow>
+        </SkillsWrapper>
+      </SkillsContainer>
+    </>
+  );
+};
+
+export default Skills;
