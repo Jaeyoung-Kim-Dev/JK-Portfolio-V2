fe2bee2b080262e6735772320a5b67d22f9ed61f
Jaeyoung Kim <jaeyoung.kim.dev@gmail.com>
2021-04-03 21:48:24 -0600

    Removed contact from navbar and fixed minor issues

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import {
   SidebarContainer,
   Icon,
@@ -9,47 +9,38 @@ import {
   SideBtnWrap,
   SidebarBtn,
 } from './SidebarElements';
+import navItems from '../Navbar/navItems.json';
 
-const Sidebar = ({ isOpen, toggle }) => {
-  const [navItems, setNavItems] = useState([]);
-
-  useEffect(() => {
-    fetch(`./JSON/navItems.json`)
-      .then((response) => response.json())
-      .then((result) => setNavItems(result));
-  }, []);
-
-  return (
-    <SidebarContainer isOpen={isOpen} onClick={toggle}>
-      <Icon onClick={toggle}>
-        <CloseIcon />
-      </Icon>
-      <SidebarWrapper>
-        <SidebarMenu>
-          {navItems.map((navItem, key) => (
-            <SidebarLink
-              key={key}
-              to={navItem.itemName.toLowerCase().replace(' ', '')}
-              offset={-document.documentElement.clientHeight * 0.1}
-              onClick={toggle}
-            >
-              {navItem.itemName}
-            </SidebarLink>
-          ))}
-        </SidebarMenu>
-        <SideBtnWrap>
-          <a
-            href={require('../../Resume(Jaeyoung_Kim).pdf')?.default}
-            target='_blank'
-            rel='noopener noreferrer'
-            download
+const Sidebar = ({ isOpen, toggle }) => (
+  <SidebarContainer isOpen={isOpen} onClick={toggle}>
+    <Icon onClick={toggle}>
+      <CloseIcon />
+    </Icon>
+    <SidebarWrapper>
+      <SidebarMenu>
+        {navItems.map((navItem, key) => (
+          <SidebarLink
+            key={key}
+            to={navItem.itemName.toLowerCase().replace(' ', '')}
+            offset={-document.documentElement.clientHeight * 0.1}
+            onClick={toggle}
           >
-            <SidebarBtn>RESUME</SidebarBtn>
-          </a>
-        </SideBtnWrap>
-      </SidebarWrapper>
-    </SidebarContainer>
-  );
-};
+            {navItem.itemName}
+          </SidebarLink>
+        ))}
+      </SidebarMenu>
+      <SideBtnWrap>
+        <a
+          href={require('../../Resume(Jaeyoung_Kim).pdf')?.default}
+          target='_blank'
+          rel='noopener noreferrer'
+          download
+        >
+          <SidebarBtn>RESUME</SidebarBtn>
+        </a>
+      </SideBtnWrap>
+    </SidebarWrapper>
+  </SidebarContainer>
+);
 
 export default Sidebar;
