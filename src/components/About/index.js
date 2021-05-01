0064c495926c87b4116281a7d9eed646e4011c05
Jaeyoung Kim <jaeyoung.kim.dev@gmail.com>
2021-04-30 23:40:49 -0600

    Added Skills to about page and changed color

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,7 +7,6 @@ import {
   AboutRow,
   Column1,
   ImgWrap,
-  BubbleWrapper,
   Bubble,
   Img,
   Column2,
@@ -15,6 +14,8 @@ import {
   Heading,
   Subtitle,
   Link,
+  TechWrapper,
+  TechIconWrapper,
 } from './AboutElements';
 
 const bubbles = [
@@ -46,9 +47,7 @@ const About = () => {
             <Column1>
               <ImgWrap>
                 <Flip left>
-                  <BubbleWrapper>
-                    <Bubble bgCorlor={bgColor}>{currentBubble}</Bubble>
-                  </BubbleWrapper>
+                  <Bubble bgCorlor={bgColor}>{currentBubble}</Bubble>
                 </Flip>
                 <Img
                   src={
@@ -61,7 +60,7 @@ const About = () => {
             </Column1>
             <Column2>
               <Fade right cascade>
-                <TopLine>ABOUT</TopLine>
+                {/* <TopLine>ABOUT</TopLine> */}
                 <Heading>Hello, everyone!</Heading>
                 <Subtitle>
                   As a software developer, I'm self-motivated, cooperative, and
@@ -87,17 +86,69 @@ const About = () => {
                   >
                     Calgary, AB, Canada
                   </Link>
-                  , I studied hard (GPA 3.89) and did some personal and team
-                  projects. You can check my projects below.
+                  , I studied hard (GPA 3.89) and won an award and a
+                  scholarship.
                 </Subtitle>
-                <Subtitle>I'm currently looking for a job! </Subtitle>
+                <Subtitle>
+                  The technologies below are the ones I'm interested in and use
+                  these days.
+                </Subtitle>
+                <TechWrapper>
+                  <TechIconWrapper
+                    href='https://developer.mozilla.org/en-US/docs/Web/JavaScript'
+                    target='_blank'
+                  >
+                    <img
+                      src='https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg'
+                      alt='javascript'
+                      width='40'
+                      height='40'
+                    />
+                  </TechIconWrapper>
+                  <TechIconWrapper
+                    href='https://www.typescriptlang.org/'
+                    target='_blank'
+                  >
+                    <img
+                      src='https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg'
+                      alt='typescript'
+                      width='40'
+                      height='40'
+                    />
+                  </TechIconWrapper>
+                  <TechIconWrapper href='https://reactjs.org/' target='_blank'>
+                    <img
+                      src='https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg'
+                      alt='react'
+                      width='40'
+                      height='40'
+                    />
+                  </TechIconWrapper>
+                  <TechIconWrapper href='https://nodejs.org' target='_blank'>
+                    <img
+                      src='https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg'
+                      alt='nodejs'
+                      width='40'
+                      height='40'
+                    />
+                  </TechIconWrapper>
+                  <TechIconWrapper href='https://www.java.com' target='_blank'>
+                    <img
+                      src='https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg'
+                      alt='java'
+                      width='40'
+                      height='40'
+                    />
+                  </TechIconWrapper>
+                </TechWrapper>
+                {/* <Subtitle>I'm currently looking for a job! </Subtitle>
                 <Subtitle>
                   Please feel free to{' '}
                   <Link href="mailto:jaeyoung.kim.dev@gmail.com?subject=Mail from Jaeyoung's Website&body=Hello, Jaeyoung!">
                     contact me
                   </Link>
                   . Thank you!
-                </Subtitle>
+                </Subtitle> */}
               </Fade>
             </Column2>
           </AboutRow>
